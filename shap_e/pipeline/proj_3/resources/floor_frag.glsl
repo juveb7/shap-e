635f8fbf89c0000cc45c66ce228d2923fbd0865f
Juvenal Francisco Barajas <juveb0111@gmail.com>
2024-05-16 21:55:53 -0700

    Delete shap_e/pipeline/proj_3/resources directory

--- shap_e/pipeline/proj_3/resources/floor_frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-in vec2 TexCoords;  // Texture coordinates from vertex shader
-out vec4 FragColor;  // Output color
-
-uniform sampler2D floorTex;  // Texture sampler for the floor
-
-void main() {
-    FragColor = texture(floorTex, TexCoords);  // Sample the texture at the passed coordinates
-}
